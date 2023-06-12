0a51051656bf1df8286aaa4342a783ad0630d774
Damian Peiris <drpeiris3@gmail.com>
2023-06-12 21:11:31 +0530

    Changed the implementation according to the use case.

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const gifUrl = "https://mir-s3-cdn-cf.behance.net/project_modules/hd/e5079365113681.5ae94b6872f65.gif";
 const gifUrlList = [];
+let numberList = [];
 let lastNumber = 0;
 $("#enqueue").on("click", () => {
     gifUrlList.push(gifUrl);
+    ++lastNumber;
+    numberList.push(lastNumber);
     let url = gifUrlList[gifUrlList.length - 1];
     $(".gifContainer").append("<img src='" + url + "' class='gifs'>");
-    $(".numberContainer").append("<h1 class='numbers'>" + ++lastNumber + "</h1>");
+    $(".numberContainer").append("<h1 class='numbers'>" + lastNumber + "</h1>");
     swal("success", "Enqueued successfully!", "success");
 });
 $("#dequeue").on("click", () => {
-    if(gifUrlList.length === 0){
+    /*This if condition never gets true , coz we rotating the array itself and
+    * never removes an element.*/
+    if (gifUrlList.length === 0 || numberList.length === 0) {
         swal("Error", "No gifs to dequeue.", "error");
-    }else{
-        gifUrlList.shift();
-        $(".gifContainer").children().first().remove();
-        $(".numberContainer").children().first().remove();
+        /*Refreshes the page if there isnt any elements in the array.*/
+        setTimeout(()=>{
+            window.location.reload()
+        },1000)
+    } else {
+        /*Removes the last element and replace it with the first element according to the use case.*/
+        gifUrlList.unshift(gifUrlList.pop());
+        numberList.unshift(numberList.pop());
+        $(".gifContainer").empty();
+        $(".numberContainer").empty();
+        numberList.map((number) => {
+            $(".numberContainer").append("<h1 class='numbers'>" + number + "</h1>");
+
+        });
+
+        gifUrlList.map((url) => {
+            $(".gifContainer").append("<img src='" + url + "' class='gifs'>");
+        });
+
+
         swal("success", "Dequeued successfully!", "success");
 
     }
